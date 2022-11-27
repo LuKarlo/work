5ea9f374a99965e33ed04858947a516d0c0d9f4a
LuKarlo <119197530+LuKarlo@users.noreply.github.com>
2022-11-27 19:01:09 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -21,6 +21,35 @@ var x = setInterval(function(){
         document.getElementById("minuti").innerHTML = "00";
         document.getElementById("secondi").innerHTML = "00";
 
-        document.getElementById('scritta').innerHTML = "Buon natale"
+        document.getElementById('scritta').innerHTML = "Buon Natale";
+    }
+},1000)
+
+var countDown2 = new Date("Jan 1, 2023 00:00:00").getTime();
+var x2 = setInterval(function(){
+    var now2 = new Date().getTime();
+    var distance2 = countDown2 - now2;
+    
+    console.log(distance2);
+
+    var giorni2 = Math.floor(distance2 / (1000*60*60*24));
+    var ore2 = Math.floor((distance2 % (1000*60*60*24)) / (1000*60*60));
+    var minuti2 = Math.floor((distance2  % (1000*60*60)) / (1000*60));
+    var secondi2 = Math.floor((distance2  % (1000*60)) / 1000);
+
+    document.getElementById("giorni2").innerHTML = giorni2;
+    document.getElementById("ore2").innerHTML = ore2;
+    document.getElementById("minuti2").innerHTML = minuti2;
+    document.getElementById("secondi2").innerHTML = secondi2;
+
+    if(distance2 < 0){
+        clearInterval(x2);
+
+        document.getElementById("giorni2").innerHTML = "00";
+        document.getElementById("ore2").innerHTML = "00";
+        document.getElementById("minuti2").innerHTML = "00";
+        document.getElementById("secondi2").innerHTML = "00";
+
+        document.getElementById('scritta2').innerHTML = "Buon Anno";
     }
 },1000)
